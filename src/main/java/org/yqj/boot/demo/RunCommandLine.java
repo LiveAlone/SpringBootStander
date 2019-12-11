3847bf3a97dc3b40b94d59c216bbcf0f44e7da80
qijunyao <qijunyao@xiaohongshu.com>
2019-12-11 14:12:55 +0800

    add MBean config

--- src/main/java/org/yqj/boot/demo/RunCommandLine.java
+++ src/main/java/org/yqj/boot/demo/RunCommandLine.java
@@ -4,6 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Description:
  *
@@ -17,6 +22,26 @@ public class RunCommandLine implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
-        log.info("run command line content null content");
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        mBeanServer.registerMBean(new ServerInfo(), new ObjectName("serverInfoMBean:name=serverInfo"));
+    }
+
+    public static class ServerInfo implements ServerInfoMBean{
+        @Override
+        public int getCmdCount(){
+            return ThreadLocalRandom.current().nextInt(1000);
+        }
+
+        @Override
+        public void printTestContent(String content) {
+            System.out.println(content);
+        }
+    }
+
+    public interface ServerInfoMBean {
+
+        int getCmdCount();
+
+        void printTestContent(String content);
     }
 }
